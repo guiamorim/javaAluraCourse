3b17d3eb56d6c14f9eef1bf89112447c3cace78a
Guilherme Amorim <guilherme.asfreitas@gmail.com>
2021-08-05 19:02:01 +0100

    Simplificação do código
    Retirada dos outputs(prints) de todos os if / else e deixando apenas um ao final do código.
    O código ficou mais limpo e organizado e não houve impacto na execução.

--- TesteIR.java
+++ TesteIR.java
@@ -8,7 +8,7 @@ public class TesteIR {
 		double deducao = 0.00;
 		
 		if (salario <= 1903.98) {
-			System.out.println("Seu rendimento se encontra fora da faixa de incidência do Imposto de renda.");
+			System.out.println("Seu rendimento se encontra fora da faixa de incidÃªncia do Imposto de renda.");
 		}
 		else {
 			if(salario >= 1903.99 && salario <= 2826.65) {
@@ -16,10 +16,6 @@ public class TesteIR {
 				ir = 7.5;
 				imposto = (salario * ir) / 100;
 				deducao = 142.80;
-				
-				System.out.println("O seu salário é R$"+ salario + ", e será atribuído uma alíquota de IR de " + ir + "%.");
-				System.out.println("Total do imposto: R$" + imposto + ".");
-				System.out.println("Permite a dedução no valor de R$" + deducao + " do imposto aplicado");
 			}
 			else {
 				if(salario >= 2826.66 && salario <= 3751.05) {
@@ -27,10 +23,6 @@ public class TesteIR {
 					ir = 15;
 					imposto = (salario * ir) / 100;
 					deducao = 354.80;
-					
-					System.out.println("O seu salário é R$"+ salario + ", e será atribuído uma alíquota de IR de " + ir + "%.");
-					System.out.println("Total do imposto: R$" + imposto + ".");
-					System.out.println("Permite a dedução no valor de R$" + deducao + " do imposto aplicado");
 				}
 				else {
 					if (salario >= 3751.06 && salario <= 4664.68) {
@@ -38,23 +30,20 @@ public class TesteIR {
 						ir = 22.5;
 						imposto = (salario * ir) / 100;
 						deducao = 636.13;
-						
-						System.out.println("O seu salário é R$"+ salario + ", e será atribuído uma alíquota de IR de " + ir + "%.");
-						System.out.println("Total do imposto: R$" + imposto + ".");
-						System.out.println("Permite a dedução no valor de R$" + deducao + " do imposto aplicado");
 					}
 					else {
 						
 						ir = 27.5;
 						imposto = (salario * ir) / 100;
-						deducao = 869.36;
-						
-						System.out.println("O seu salário é R$"+ salario + ", e será atribuído uma alíquota de IR de " + ir + "%.");
-						System.out.println("Total do imposto: R$" + imposto + ".");
-						System.out.println("Permite a dedução no valor de R$" + deducao + " do imposto aplicado");
+						deducao = 869.36;	
 					}
 				}
 			}
 		}
+		
+		System.out.println("O seu salÃ¡rio Ã© R$"+ salario + ", e serÃ¡ atribuÃ­do uma alÃ­quota de IR de " + ir + "%.");
+		System.out.println("Total do imposto: R$" + imposto + ".");
+		System.out.println("Permite a deduÃ§Ã£o no valor de R$" + deducao + " do imposto aplicado");
+		
 	}
 }
