21f219637bbdfb537c8bf7c7b854bf44f2b80979
Guilherme Amorim <guilherme.asfreitas@gmail.com>
2021-08-05 09:52:09 +0100

    Char
    Usando variavel primitiva char

---
+++ TestaCaracteres.java
@@ -0,0 +1,18 @@
+
+public class TestaCaracteres {
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		char valor2 = (char)(valor + 2);
+		System.out.println(valor2);
+		
+		String palavra = ("Isso é um teste de String");
+		System.out.println(palavra);
+		
+		System.out.println(palavra + 2020);
+	}
+}
