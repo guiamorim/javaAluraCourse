e7d2921a9faabb57c75534c6daa462ec04e10a38
Guilherme Amorim <guilherme.asfreitas@gmail.com>
2021-08-07 22:34:32 +0100

    Teste de laço usando o 'While'

---
+++ TestaWhile.java
@@ -0,0 +1,20 @@
+
+public class TestaWhile {
+	public static void main(String[] args) {
+		
+		int contador = 0; //declaração e inicialização da variável
+		
+		while(contador <= 10) {
+			System.out.println(contador);
+			
+			//tipos variados de incrementos
+			//contador = contador + 1; 
+			//contador += 1;
+			//++contador; 
+			
+			contador ++;
+		}
+		
+		System.out.println(contador);
+	}
+}
